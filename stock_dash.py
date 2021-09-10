113040ca00c6cc1e6357a0539727147be86da4c0
AtharvaKatre <45846529+AtharvaKatre@users.noreply.github.com>
2021-09-10 14:04:17 +0530

    Update stock_dash.py

--- stock_dash.py
+++ stock_dash.py
@@ -3,7 +3,7 @@ import plotly.graph_objs as go
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
-import dash_html_components as html
+import dash_html_components as html 
 import dash_table
 from dash.dependencies import Input, Output
 
